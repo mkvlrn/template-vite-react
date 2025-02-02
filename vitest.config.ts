c9425a122e00710152229802b6cc9a35f80aad24
Mike Valeriano <mkvlrn@proton.me>
2025-02-02 02:12:30 -0300

    chore: adjust vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["./src/**/*.test.{ts,tsx}"],
+    include: ["src/**/*.test.{ts,tsx}"],
     reporters: ["verbose"],
     watch: false,
     coverage: {
       all: true,
       clean: true,
       cleanOnRerun: true,
-      reportsDirectory: "coverage",
-      reporter: ["lcov", "html", "text"],
       include: ["src"],
       exclude: ["src/**/*.test.{ts,tsx}", "src/main.{ts,tsx}"],
     },
