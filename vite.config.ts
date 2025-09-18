1980d19a07914b636abd3722048ce0141f5c1015
Mike Valeriano <mkvlrn@gmail.com>
2025-09-18 19:09:13 -0300

    chore: bump deps

--- vite.config.ts
+++ vite.config.ts
@@ -5,42 +5,51 @@ import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
 
 const { PORT = "3000", NODE_ENV } = process.env;
 
-const config = defineConfig({
-  base: NODE_ENV === "production" ? "https://mkvlrn.github.io/template-vite-react/" : "",
+function getConfig() {
+  const config = defineConfig({
+    base: NODE_ENV === "production" ? "https://mkvlrn.github.io/template-vite-react/" : "",
 
-  plugins: [react()],
+    plugins: [react()],
 
-  server: {
-    port: Number(PORT),
-    allowedHosts: true,
-  },
-
-  build: {
-    target: "esnext",
-    sourcemap: true,
-    outDir: "./build",
-    emptyOutDir: true,
-  },
-});
-
-const testConfig = defineTestConfig({
-  test: {
-    include: ["src/**/*.test.{ts,tsx}"],
-    reporters: ["verbose"],
-    watch: false,
-    coverage: {
-      all: true,
-      clean: true,
-      cleanOnRerun: true,
-      include: ["src"],
-      exclude: ["src/**/*.test.{ts,tsx}", "src/main.{ts,tsx}"],
+    server: {
+      port: Number(PORT),
+      allowedHosts: true,
     },
-    // biome-ignore lint/style/useNamingConvention: needed for vitest
-    env: { NODE_ENV: "test" },
-    environment: "jsdom",
-    passWithNoTests: true,
-    setupFiles: ["./vitest.setup.ts"],
-  },
-});
 
-export default mergeConfig(config, testConfig);
+    build: {
+      target: "esnext",
+      sourcemap: true,
+      outDir: "./build",
+      emptyOutDir: true,
+    },
+  });
+
+  const withIntegration = process.env["INTEGRATION"] === "true";
+  const testFiles = withIntegration
+    ? ["./src/**/*.{test,spec}.{ts,tsx}"]
+    : ["./src/**/*.test.{ts,tsx}"];
+
+  const testConfig = defineTestConfig({
+    test: {
+      include: testFiles,
+      reporters: ["verbose"],
+      watch: false,
+      coverage: {
+        all: true,
+        clean: true,
+        cleanOnRerun: true,
+        include: ["src"],
+        exclude: ["src/**/*.{test,spec}.{ts,tsx}", "src/main.{ts,tsx}"],
+      },
+      // biome-ignore lint/style/useNamingConvention: needed for vitest
+      env: { NODE_ENV: "test" },
+      environment: "jsdom",
+      passWithNoTests: true,
+      setupFiles: ["./vitest.setup.ts"],
+    },
+  });
+
+  return mergeConfig(config, testConfig);
+}
+
+export default getConfig();
