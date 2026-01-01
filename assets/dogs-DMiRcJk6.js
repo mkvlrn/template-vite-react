1ef1b217fa6fc9b1cb51d88de5631cecbab340cb
mkvlrn <mkvlrn@users.noreply.github.com>
2026-01-01 01:59:41 +0000

    deploy: 56a402ab7e930fec24993959b686caf338c5e49f

---
+++ assets/dogs-DMiRcJk6.js
@@ -0,0 +1,2 @@
+import{j as o}from"./index-B5A7yWEo.js";import{P as t}from"./pets-B6SF-RVS.js";function e(){return o.jsx(t,{type:"dog"})}export{e as component};
+//# sourceMappingURL=dogs-DMiRcJk6.js.map
