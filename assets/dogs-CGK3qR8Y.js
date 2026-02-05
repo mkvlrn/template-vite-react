06106fc5188a5711a6d60d557ef812e3e6b6cfc4
mkvlrn <mkvlrn@users.noreply.github.com>
2026-02-05 10:25:18 +0000

    deploy: cf1971b3b7994ed226b13fe67d49df169ce310b3

---
+++ assets/dogs-CGK3qR8Y.js
@@ -0,0 +1,2 @@
+import{j as o}from"./index-C4gsm66G.js";import{P as t}from"./pets-CWiyP2PB.js";function e(){return o.jsx(t,{type:"dog"})}export{e as component};
+//# sourceMappingURL=dogs-CGK3qR8Y.js.map
