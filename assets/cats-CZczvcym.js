4075f1a0f6f8ed131e26bf7cbb69eb9ee4d92590
mkvlrn <mkvlrn@users.noreply.github.com>
2026-02-25 15:08:10 +0000

    deploy: 3913b23afe80b45f93d45e3e864dba0baf53b78b

---
+++ assets/cats-CZczvcym.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-Cm4JVzcp.js";import{P as o}from"./pets-xA6_uawM.js";function e(){return t.jsx(o,{type:"cat"})}export{e as component};
+//# sourceMappingURL=cats-CZczvcym.js.map
---
+++ assets/dogs-BCdPxXQJ.js
@@ -0,0 +1,2 @@
+import{j as o}from"./index-Cm4JVzcp.js";import{P as t}from"./pets-xA6_uawM.js";function e(){return o.jsx(t,{type:"dog"})}export{e as component};
+//# sourceMappingURL=dogs-BCdPxXQJ.js.map
