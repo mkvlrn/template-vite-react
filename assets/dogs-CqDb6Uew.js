163b2ed8df8e39e6eaf73dbc9a06e5c4f9ea0627
mkvlrn <mkvlrn@users.noreply.github.com>
2026-03-19 19:24:45 +0000

    deploy: 2820a9dc150df75b610dd12684e95728cce78129

---
+++ assets/dogs-CqDb6Uew.js
@@ -0,0 +1,2 @@
+import{t as e}from"./jsx-runtime-DAs1UGHr.js";import{t}from"./pets-Dm70WHbU.js";var n=e();function r(){return(0,n.jsx)(t,{type:`dog`})}export{r as component};
+//# sourceMappingURL=dogs-CqDb6Uew.js.map
