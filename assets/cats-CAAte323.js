e5026d1f3428d7c6e18eb08c168c6c073d91a639
mkvlrn <mkvlrn@users.noreply.github.com>
2026-03-26 07:00:55 +0000

    deploy: 95eb31774adabf4dab8e576b6e571356000127ac

---
+++ assets/cats-CAAte323.js
@@ -0,0 +1,2 @@
+import{t as e}from"./jsx-runtime-DAs1UGHr.js";import{t}from"./pets-C9YJ-a6f.js";var n=e();function r(){return(0,n.jsx)(t,{type:`cat`})}export{r as component};
+//# sourceMappingURL=cats-CAAte323.js.map
