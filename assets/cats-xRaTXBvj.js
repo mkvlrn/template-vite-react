6ace70290a07be138d8f08e956b6cbf4371e386b
mkvlrn <mkvlrn@users.noreply.github.com>
2026-04-02 03:59:27 +0000

    deploy: d176417fd4e709d266d5842a828de3222d75b0e4

---
+++ assets/cats-xRaTXBvj.js
@@ -0,0 +1,2 @@
+import{t as e}from"./jsx-runtime-DAs1UGHr.js";import{t}from"./pets-DvUFr6Cl.js";var n=e();function r(){return(0,n.jsx)(t,{type:`cat`})}export{r as component};
+//# sourceMappingURL=cats-xRaTXBvj.js.map
