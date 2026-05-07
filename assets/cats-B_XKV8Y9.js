79156516ef593dd4a8f9e64845ea88448c4754a7
mkvlrn <mkvlrn@users.noreply.github.com>
2026-05-07 15:28:17 +0000

    deploy: 0c67a940d2e9dd056119ae41ba782b02de6e68e5

---
+++ assets/cats-B_XKV8Y9.js
@@ -0,0 +1,2 @@
+import{t as e}from"./jsx-runtime-BOY_dY-B.js";import{t}from"./pets-VVfeQNNA.js";var n=e();function r(){return(0,n.jsx)(t,{type:`cat`})}export{r as component};
+//# sourceMappingURL=cats-B_XKV8Y9.js.map
---
+++ assets/dogs-CEXSPNU9.js
@@ -0,0 +1,2 @@
+import{t as e}from"./jsx-runtime-BOY_dY-B.js";import{t}from"./pets-VVfeQNNA.js";var n=e();function r(){return(0,n.jsx)(t,{type:`dog`})}export{r as component};
+//# sourceMappingURL=dogs-CEXSPNU9.js.map
