6b020ea2333612a4806652682ac950c94da982fa
mkvlrn <mkvlrn@users.noreply.github.com>
2026-07-12 15:06:57 +0000

    deploy: ba0a22b7712bcee6f73b4ba5282e505c9d72f58f

---
+++ assets/dogs-D0PZ1U7j.js
@@ -0,0 +1,2 @@
+import{t as e}from"./pets-C0LlwtGc.js";import{n as t}from"./index-BgipWFYb.js";var n=t();function r(){return(0,n.jsx)(e,{type:`dog`})}export{r as component};
+//# sourceMappingURL=dogs-D0PZ1U7j.js.map
