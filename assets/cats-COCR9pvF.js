58544df135a5173eaa658d0d37244e53c9c6f909
mkvlrn <mkvlrn@users.noreply.github.com>
2026-07-26 07:28:02 +0000

    deploy: 7af1afb8acf6f98b99271ff7f8f9d73246840c04

--- assets/cats-COCR9pvF.js
+++
@@ -1,2 +0,0 @@
-import{t as e}from"./pets-C0LlwtGc.js";import{n as t}from"./index-BgipWFYb.js";var n=t();function r(){return(0,n.jsx)(e,{type:`cat`})}export{r as component};
-//# sourceMappingURL=cats-COCR9pvF.js.map
---
+++ assets/dogs-CcmUn0sB.js
@@ -0,0 +1,2 @@
+import{t as e}from"./pets-CfiX9MoL.js";import{n as t}from"./index-CfuNsDfX.js";var n=t();function r(){return(0,n.jsx)(e,{type:`dog`})}export{r as component};
+//# sourceMappingURL=dogs-CcmUn0sB.js.map
