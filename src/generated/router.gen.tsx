7af1afb8acf6f98b99271ff7f8f9d73246840c04
mkvlrn <mkvlrn@gmail.com>
2026-07-26 04:26:41 -0300

    chore: bump deps

--- src/generated/router.gen.tsx
+++ src/generated/router.gen.tsx
@@ -9,13 +9,13 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './../routes/__root'
-import { Route as DogsRouteImport } from './../routes/dogs'
-import { Route as CatsRouteImport } from './../routes/cats'
 import { Route as IndexRouteImport } from './../routes/index'
+import { Route as CatsRouteImport } from './../routes/cats'
+import { Route as DogsRouteImport } from './../routes/dogs'
 
-const DogsRoute = DogsRouteImport.update({
-  id: '/dogs',
-  path: '/dogs',
+const IndexRoute = IndexRouteImport.update({
+  id: '/',
+  path: '/',
   getParentRoute: () => rootRouteImport,
 } as any)
 const CatsRoute = CatsRouteImport.update({
@@ -23,9 +23,9 @@ const CatsRoute = CatsRouteImport.update({
   path: '/cats',
   getParentRoute: () => rootRouteImport,
 } as any)
-const IndexRoute = IndexRouteImport.update({
-  id: '/',
-  path: '/',
+const DogsRoute = DogsRouteImport.update({
+  id: '/dogs',
+  path: '/dogs',
   getParentRoute: () => rootRouteImport,
 } as any)
 
@@ -61,11 +61,11 @@ export interface RootRouteChildren {
 
 declare module '@tanstack/react-router' {
   interface FileRoutesByPath {
-    '/dogs': {
-      id: '/dogs'
-      path: '/dogs'
-      fullPath: '/dogs'
-      preLoaderRoute: typeof DogsRouteImport
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/cats': {
@@ -75,11 +75,11 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof CatsRouteImport
       parentRoute: typeof rootRouteImport
     }
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexRouteImport
+    '/dogs': {
+      id: '/dogs'
+      path: '/dogs'
+      fullPath: '/dogs'
+      preLoaderRoute: typeof DogsRouteImport
       parentRoute: typeof rootRouteImport
     }
   }
