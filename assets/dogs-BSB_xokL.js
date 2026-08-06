58f24ca0fc3789b4f2bb1c14659e737173fcfe33
mkvlrn <mkvlrn@users.noreply.github.com>
2026-08-06 12:27:24 +0000

    deploy: 3545ba7747c558a9eb7aa2612209e524bbccfb78

--- assets/dogs-BSB_xokL.js
+++
@@ -1,2 +0,0 @@
-import{t as e}from"./pets-DJiwlFS2.js";import{n as t}from"./index-C9JRR6ET.js";var n=t();function r(){return(0,n.jsx)(e,{type:`dog`})}export{r as component};
-//# sourceMappingURL=dogs-BSB_xokL.js.map
---
+++ assets/dogs-BrHrfHXl.js
@@ -0,0 +1,2 @@
+import{t as e}from"./pets-D2oXxj0p.js";import{n as t}from"./index-DMXPq7_n.js";var n=t();function r(){return(0,n.jsx)(e,{type:`dog`})}export{r as component};
+//# sourceMappingURL=dogs-BrHrfHXl.js.map
