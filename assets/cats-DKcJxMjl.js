39196f413f0aae75e51abf912139c4114bcec98a
mkvlrn <mkvlrn@users.noreply.github.com>
2026-08-07 16:47:56 +0000

    deploy: 5fc3a539a0586b8073b32092cf1e10291ba405d8

--- assets/cats-DKcJxMjl.js
+++
@@ -1,2 +0,0 @@
-import{t as e}from"./pets-D2oXxj0p.js";import{n as t}from"./index-DMXPq7_n.js";var n=t();function r(){return(0,n.jsx)(e,{type:`cat`})}export{r as component};
-//# sourceMappingURL=cats-DKcJxMjl.js.map
---
+++ assets/dogs-Dgk08DB0.js
@@ -0,0 +1,2 @@
+import{t as e}from"./pets-ibvUcRJL.js";import{n as t}from"./index-BoH-ZAS6.js";var n=t();function r(){return(0,n.jsx)(e,{type:`dog`})}export{r as component};
+//# sourceMappingURL=dogs-Dgk08DB0.js.map
