511de5a172613bf317fcc52dda46af6cad30f1ac
mkvlrn <mkvlrn@users.noreply.github.com>
2026-09-05 22:08:49 +0000

    deploy: ee85a6f791540fec744a98f51293a05cb792e92a

---
+++ assets/dogs-CyPJvh0I.js
@@ -0,0 +1,2 @@
+import{t as e}from"./pets-BjhVImjz.js";import{h as t}from"./index-2K58AVdI.js";var n=t();function r(){return(0,n.jsx)(e,{type:`dog`})}export{r as component};
+//# sourceMappingURL=dogs-CyPJvh0I.js.map
