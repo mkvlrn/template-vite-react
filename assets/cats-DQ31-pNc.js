8259c7173f2c8a26465da30f3bd6ff30c308ec07
mkvlrn <mkvlrn@users.noreply.github.com>
2026-09-07 18:30:02 +0000

    deploy: df358d413be349139d13442b0e3ba79b2e2e71a6

---
+++ assets/cats-DQ31-pNc.js
@@ -0,0 +1,2 @@
+import{t as e}from"./pets-CBueTFRo.js";import{h as t}from"./index-pic1kb5i.js";var n=t();function r(){return(0,n.jsx)(e,{type:`cat`})}export{r as component};
+//# sourceMappingURL=cats-DQ31-pNc.js.map
